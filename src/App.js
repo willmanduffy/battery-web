576b6eed4f3a407725d2c0b2a7a0cbe7ca6a23c0
Kilian McMahon <kil@kilmc.com>
2017-06-09 14:15:25 -0400

    Style it up all nice

--- src/App.js
+++ src/App.js
@@ -14,29 +14,35 @@ const App = ({ state, dispatch }) => {
 
     const attributes = datum.css.map((cssItem) => {
       return (
-        <p key={`${datum.className}-${cssItem.cssProp}`} className='pl1'>{cssItem.cssProp}: {cssItem.cssValue}</p>
+        <p key={`${datum.className}-${cssItem.cssProp}`} className='pl3'>{cssItem.cssProp}: {cssItem.cssValue}</p>
       );
     });
 
     return(
-      <li className={`p2 ${backgroundStyle}`} key={index}>
-        <p>{`${datum.className} {`}</p>
-        {attributes}
-        <p>{`}`}</p>
+      <li className={`p2 lh2 ${backgroundStyle}`} key={index}>
+        <div className='lh2'>
+          <span>{`.${datum.className} `}</span>
+          <span>{`{`}</span>
+        </div>
+        <div className='lh2'>{attributes}</div>
+        <div className='lh2'>{`}`}</div>
       </li>
     )
   });
 
   return(
-    <div>
-      <input
-        onChange={event => dispatch(searchChanged(event.target.value))}
-        className='w100p p2'
-      />
+    <div className="bg-grey-200 mh100vh pt6 border-box">
+      <div className='w80p mx-auto p4 bg-white shadow-2 rounded'>
+        <h2 className="light fz-48 mt0 mb3">Atomic Classes</h2>
+        <input
+          onChange={event => dispatch(searchChanged(event.target.value))}
+          className='w100p p2 border-box rounded border border-grey-400 outline-none fz-16'
+        />
 
-      <ul className='pl0 list-none'>
-        {results}
-      </ul>
+        <ul className='pl0 list-none type-mono'>
+          {results}
+        </ul>
+      </div>
     </div>
   );
 };
